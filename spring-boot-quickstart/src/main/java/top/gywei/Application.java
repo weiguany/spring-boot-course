73cdce8e4d73baac6b6306a0d3250c00e90f4956
weiguanyu <1217663021@qq.com>
2025-09-06 15:40:57 +0800

    init:初始化父子工程学习仓库，并编写第一个接口

--- spring-boot-quickstart/src/main/java/top/gywei/Application.java
+++ spring-boot-quickstart/src/main/java/top/gywei/Application.java
@@ -1,4 +1,4 @@
-package top.gyliu;
+package top.gywei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        System.out.println("http://localhost:8081/hello");
+        System.out.println("http://localhost:8088/hello");
+        System.out.println("http://localhost:8088/mood");
     }
 }
